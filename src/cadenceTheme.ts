ce2cf53bcd2b7ebdc2c725caeab05e8b33927f57
Max Daunarovich <max.daunarovich@gmail.com>
2020-05-25 23:08:57 +0300

    Create separate file for Cadence theme

---
+++ src/cadenceTheme.ts
@@ -0,0 +1,14 @@
+export default {
+  base: 'vs',
+  inherit: true,
+  rules: [{ background: 'EDF9FA' }],
+  colors: {
+    'editor.foreground': '#000000',
+    'editor.background': '#EDF9FA',
+    'editorCursor.foreground': '#8B0000',
+    'editor.lineHighlightBackground': '#0000FF20',
+    'editorLineNumber.foreground': '#008800',
+    'editor.selectionBackground': '#88000030',
+    'editor.inactiveSelectionBackground': '#88000015',
+  },
+};
