f7b1766ced7d3e6b5506213cdbec87c0edbf0638
Max Daunarovich <max.daunarovich@gmail.com>
2021-01-21 13:21:44 +0300

    Define theme list

---
+++ src/themeList.ts
@@ -0,0 +1,10 @@
+import cadenceTheme from './cadenceTheme';
+
+export const flowTheme = cadenceTheme
+export const monokaiBright = require('monaco-themes/themes/Monokai.json');
+
+export const list = [
+    'Flow Theme',
+    'Dark - Monokai Bright',
+    'Dark - Some Other Theme'
+]
